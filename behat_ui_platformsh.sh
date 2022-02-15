a75c368fa511f66790225cc26c8edf0337d8a920
Rajab Natshah <rajabn@gmail.com>
2022-02-15 22:05:23 +0200

    Add a Web command to setup Behat UI module and configs for Platform.sh projects #7

--- behat_ui_platformsh.sh
+++ behat_ui_platformsh.sh
@@ -21,6 +21,9 @@ echo "  #                     Behat UI Platform.sh                           #";
 echo "  ######################################################################";
 echo "                                                                        ";
 
+## Default Selenium host.
+selenium_host='robot1.dev.in.vardot.com:4445/wd/hub';
+
 current_path=$(pwd) ;
 current_project_name_from_path=${PWD##*/} ;
 
@@ -83,6 +86,23 @@ while [[ ! ${platformsh_project_base_url} =~ $url_format ]]; do
   fi
 done
 
+## Grab Selenium host domain argument.
+unset platformsh_selenium_host;
+while [[ ! ${platformsh_selenium_host} =~ $domain_format ]]; do
+
+  read -p "Selenium Host domain ( ${selenium_host} ): " platformsh_selenium_host;
+
+  if [ -z "$platformsh_selenium_host" ]
+  then
+    platformsh_selenium_host=${selenium_host};
+  fi
+
+  if [[ ! ${platformsh_selenium_host} =~ $domain_format ]]; then
+    echo "---------------------------------------------------------------------------";
+    echo "  The Project base url is not a valid Platform.sh project link !";
+    echo "---------------------------------------------------------------------------";
+  fi
+done
 
 ## Change directory to the local project path.
 cd $platformsh_local_project_path ;
@@ -91,7 +111,7 @@ cd $platformsh_local_project_path ;
 composer require 'drupal/behat_ui:~4.0' --dev ;
 
 ## Download platformsh_behat_ui and place target folders and files.
-version="1.0.1";
+version="1.0.2";
 if [[ -f "${platformsh_local_project_path}/${version}.tar.gz" ]]; then
   rm ${platformsh_local_project_path}/${version}.tar.gz ;
 fi
@@ -118,8 +138,14 @@ mv ${platformsh_local_project_path}/${version}/behat.yml ${platformsh_local_proj
 sudo rm -rf ${platformsh_local_project_path}/${version}.tar.gz ${platformsh_local_project_path}/${version} ;
 sudo rm -rf ${platformsh_local_project_path}/wget-log* ;
 
-# Replace all PROJECT_NAME with the machine name of the Platform.sh project folder name.
-grep -rl 'PROJECT_NAME' ${platformsh_local_project_path}/features | xargs sed -i "s|PROJECT_NAME|${platformsh_project_name}|g" ;
+# Replace PLATFORMSH_PROJECT_PATH with the Platform.sh project path.
+grep -rl "PLATFORMSH_PROJECT_PATH" ${platformsh_local_project_path}/features | xargs sed -i "s|PLATFORMSH_PROJECT_PATH|${platformsh_local_project_path}|g" ;
 
-# Replace all PROJECT_BASE_URL  of Platform.sh Project URL.
-grep -rl 'PROJECT_BASE_URL' ${platformsh_local_project_path}/features | xargs sed -i "s|PROJECT_BASE_URL|${platformsh_project_base_url}|g" ;
+# Replace PROJECT_NAME with the machine name of the Platform.sh project folder name.
+grep -rl "PROJECT_NAME" ${platformsh_local_project_path}/features | xargs sed -i "s|PROJECT_NAME|${platformsh_project_name}|g" ;
+
+# Replace PROJECT_BASE_URL of Platform.sh Project URL.
+grep -rl "PROJECT_BASE_URL" ${platformsh_local_project_path}/features | xargs sed -i "s|PROJECT_BASE_URL|${platformsh_project_base_url}|g" ;
+
+# Replace SELENIUM_HOST with the current selected selenium host domain.
+grep -rl "SELENIUM_HOST" ${platformsh_local_project_path}/features | xargs sed -i "s|SELENIUM_HOST|${platformsh_selenium_host}|g" ;
