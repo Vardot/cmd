21aba69eebaba14634745cf2777f17530760926b
devops-vardot <98961472+devops-vardot@users.noreply.github.com>
2022-02-20 19:43:18 +0200

    Change the target featuers to docroot/sites/default/files

--- behat_ui_platformsh.sh
+++ behat_ui_platformsh.sh
@@ -43,7 +43,7 @@ platformsh_local_project_path=${current_path};
 unset platformsh_project_base_url;
 while [[ ! ${platformsh_project_base_url} =~ $url_format ]]; do
 
-  read -p "Project base url ( https://www.autotest-#######-##########.####.platformsh.site/): " platformsh_project_base_url;
+  read -p "Project base testing url ( https://www.autotest-#######-##########.####.platformsh.site): " platformsh_project_base_url;
 
   if [[ ! ${platformsh_project_base_url} =~ $url_format ]]; then
     echo "---------------------------------------------------------------------------";
@@ -77,7 +77,7 @@ cd $platformsh_local_project_path ;
 composer require 'drupal/behat_ui:~4.0' --dev ;
 
 ## Download platformsh_behat_ui and place target folders and files.
-version="1.0.5";
+version="1.0.7";
 if [[ -f "${platformsh_local_project_path}/${version}.tar.gz" ]]; then
   rm ${platformsh_local_project_path}/${version}.tar.gz ;
 fi
@@ -90,18 +90,23 @@ if [[ -f "${platformsh_local_project_path}/behat.yml" ]]; then
   rm ${platformsh_local_project_path}/behat.yml;
 fi
 
-if [[ -d "${platformsh_local_project_path}/features" ]]; then
-  sudo rm -rf ${platformsh_local_project_path}/features ;
+if [[ -d "${platformsh_local_project_path}/docroot/sites/default/files/features" ]]; then
+  sudo rm -rf ${platformsh_local_project_path}/docroot/sites/default/files/features ;
 fi
 
+## Get the selected version for platform.sh Behat UI tamplate.
 wget https://bitbucket.org/Vardot/platformsh_behat_ui/get/${version}.tar.gz;
 mkdir ${platformsh_local_project_path}/${version};
 tar -xzvf ${platformsh_local_project_path}/${version}.tar.gz --strip 1 --directory=${platformsh_local_project_path}/${version};
 
+## Copy Behat UI settings to config/sync
+mv ${platformsh_local_project_path}/${version}/features/behat_ui.settings.yml ${platformsh_local_project_path}/config/sync/ ;
+
 
 ## Place features folder in its target path.
-mv ${platformsh_local_project_path}/${version}/features ${platformsh_local_project_path}/features;
-sudo chmod -R 775 ${platformsh_local_project_path}/features ;
+mkdir -p ${platformsh_local_project_path}/docroot/sites/default/files;
+mv ${platformsh_local_project_path}/${version}/features ${platformsh_local_project_path}/docroot/sites/default/files/features;
+sudo chmod -R 775 ${platformsh_local_project_path}/docroot/sites/default/files/features;
 
 ## Place behat.yml file in its target path.
 mv ${platformsh_local_project_path}/${version}/behat.yml ${platformsh_local_project_path}/behat.yml;
